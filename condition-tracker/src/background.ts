e264a48e85f5ae7e7e1f6d507ad2954b57b7f8cb
keegberg <95367836+keegberg@users.noreply.github.com>
2023-06-28 13:45:27 -0500

    initial commit

---
+++ condition-tracker/src/background.ts
@@ -0,0 +1,37 @@
+import OBR from "@owlbear-rodeo/sdk";
+import { getPluginId } from "./getPluginId";
+
+import icon from "./status.svg";
+
+/**
+ * This file represents the background script run when the plugin loads.
+ * It creates the context menu item for the status ring.
+ */
+
+OBR.onReady(() => {
+  OBR.contextMenu.create({
+    id: getPluginId("menu"),
+    icons: [
+      {
+        icon,
+        label: "Condition Tracker",
+        filter: {
+          every: [
+            { key: "type", value: "IMAGE" },
+            { key: "layer", value: "CHARACTER" },
+          ],
+          permissions: ["UPDATE"],
+        },
+      },
+    ],
+    onClick(_, elementId) {
+      OBR.popover.open({
+        id: getPluginId("color-picker"),
+        url: "/",
+        height: 88,
+        width: 216,
+        anchorElementId: elementId,
+      });
+    },
+  });
+});
---
+++ condition-tracker/src/conditions.ts
@@ -0,0 +1,18 @@
+/** Conditions */
+export const conditions = [
+  "Blinded",
+  "Charmed",
+  "Deafened",
+  "Exhausted",
+  "Frightened",
+  "Grappled",
+  "Incapacitated",
+  "Invisible",
+  "Paralyzed",
+  "Petrified",
+  "Poisoned",
+  "Prone",
+  "Restrained",
+  "Stunned",
+  "Unconscious",
+];
